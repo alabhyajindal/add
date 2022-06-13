fd0fbca474bac4592712217ad8e94a7b6ccb86cb
Alabhya Jindal <alabhya10@gmail.com>
2022-06-13 23:51:51 +0530

    functional

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
+
+// #[wasm_bindgen]
+// pub fn greet(name: &str) {
+//     alert(&format!("Hello, {}!", name))
+// }
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    let result =  a + b;
+    return result;
+    // alert(&format!("The result is {}!", result))
+}
+
+
