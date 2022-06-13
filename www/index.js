22571d9dccc515ff2847a94b5608bd1181694f3a
Alabhya Jindal <alabhya10@gmail.com>
2022-06-14 00:01:08 +0530

    added the web files

---
+++ www/index.js
@@ -0,0 +1,24 @@
+import * as wasm from 'wasm';
+
+// Selecting DOM Elements
+const btn = document.querySelector('button');
+const inputA = document.querySelector('#a');
+const inputB = document.querySelector('#b');
+const result = document.querySelector('p');
+
+function sendToRust() {
+  const valueA = Number(inputA.value);
+  const valueB = Number(inputB.value);
+
+  // Error handling in case the input fields are empty
+  if (valueA != '' && valueB != '') {
+    const addedValues = wasm.add(valueA, valueB);
+    result.textContent = addedValues;
+  } else {
+    result.textContent = 'Please enter values you want to add';
+  }
+}
+
+btn.addEventListener('click', () => {
+  sendToRust();
+});
